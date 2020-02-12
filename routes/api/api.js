568cf85c03469bc3b394b4843f05cac4b50f891d
JoseCarlosMezaMadrid (JoseMeza) <josecm9607@gmail.com>
2020-02-11 19:51:36 -0600

    Primer Commit del proyecto

---
+++ routes/api/api.js
@@ -0,0 +1,17 @@
+
+    var express = require('express');
+    var router = express.Router();
+    
+    router.get( '/', (req, res)=>{
+           res.status(200).json({"msg":"API v1"});
+    });
+
+   module.exports = router;
+
+
+
+
+
+
+
+
